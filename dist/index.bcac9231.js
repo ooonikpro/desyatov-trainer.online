82b3d8e10b200ddf43b284b14c57cb9e35bcc8d8
Ovechkin Nikita <pumpingmonster04b63@gmail.com>
2024-04-05 16:02:08 +0200

    feat: github publish

---
+++ dist/index.bcac9231.js
@@ -0,0 +1,2 @@
+let t=0;const e=document.querySelectorAll(".text-animation"),o=()=>{e[t].classList.toggle("d-none"),t<e.length-1?t++:t=0,e[t].classList.toggle("d-none"),setTimeout(o,1500)};setTimeout(o,1500);
+//# sourceMappingURL=index.bcac9231.js.map
